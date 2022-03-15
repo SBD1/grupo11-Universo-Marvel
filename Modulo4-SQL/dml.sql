ea62db876d5df4c542ffae83accd50ce7e2f2176
Julio Litwin <j.litwin@live.com>
2022-03-15 15:21:45 -0300

    Update dml.sql

--- Modulo4-SQL/dml.sql
+++ Modulo4-SQL/dml.sql
@@ -7,7 +7,9 @@ INSERT INTO Joia (Id, Nome, Descricao) VALUES
 
 INSERT INTO Mapa (Nome, Ano, Altura, Largura, Requisito) VALUES
 ('Nova Iorque', 2022, 10, 5, NULL),
-('Nova Iorque', 1984, 10, 5, currval('item_id_seq'));
+('Nova Iorque', 1984, 10, 5, currval('item_id_seq'),
+('Nova Iorque', 1970, 10, 5, NULL),
+('Eternos', 2022, 10, 5, NULL));
 
 INSERT INTO Personagem (Tipo) VALUES
 ('H');
@@ -20,6 +22,16 @@ INSERT INTO Heroi (Id, Nome, AgilidadeBase, VidaBase, Latitude, Longitude, Mapa)
 (currval('personagem_id_seq'), 'Capitão América', 25, 150, 7, 3, 2);
 
 INSERT INTO Personagem (Tipo) VALUES
+('H');
+INSERT INTO Heroi (Id, Nome, AgilidadeBase, VidaBase, Latitude, Longitude, Mapa) VALUES
+(currval('personagem_id_seq'), 'Homem de Ferro', 25, 250, 8, 4, 3);
+
+INSERT INTO Personagem (Tipo) VALUES
 ('V');
 INSERT INTO Vilao (Id, Nome, Agilidade, Vida, Experiencia, Dano_Maximo, Dano_Critico, Rolagens, Defesa, Latitude, Longitude, Mapa) VALUES
 (currval('personagem_id_seq'), 'Duende Verde', 35, 120, 200, 15, 200, 5, 15, 8, 3, 1);
+
+INSERT INTO Personagem (Tipo) VALUES
+('V');
+INSERT INTO Vilao (Id, Nome, Agilidade, Vida, Experiencia, Dano_Maximo, Dano_Critico, Rolagens, Defesa, Latitude, Longitude, Mapa) VALUES
+(currval('personagem_id_seq'), 'Thanos', 999, 999, 999, 999, 999, 99, 99, 1, 1, 4);
