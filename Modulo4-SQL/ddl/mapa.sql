8262da9a6190551ec80c1e7ca4f158c79a7c131e
hericklima22 <herick.lima77@gmail.com>
2022-03-28 15:17:01 -0300

    algumas armas adicionadas

--- Modulo4-SQL/ddl/mapa.sql
+++ Modulo4-SQL/ddl/mapa.sql
@@ -3,4 +3,4 @@ INSERT INTO mapa (nome, ano, altura, largura) VALUES
 ('Nova York', 2012, 89, 51),
 ('Base da S.H.I.E.L.D.', 1970, 66, 88),
 ('Vormir', 2014, 91, 55),
-('Morag', 2014, 61, 78),
+('Morag', 2014, 61, 78);
