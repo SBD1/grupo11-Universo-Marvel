9510fe5cbd7599f960b86a21248002a13ec3b881
hericklima22 <herick.lima77@gmail.com>
2022-03-28 15:58:13 -0300

    mais alguns viloes

--- Modulo4-SQL/ddl/vilao.sql
+++ Modulo4-SQL/ddl/vilao.sql
@@ -1,6 +1,5 @@
 INSERT INTO vilao (nome, agilidade, vida, experiencia, dano_maximo, dano_critico, rolagens, defesa) VALUES
 ('Thanos', 65, 100, 50, 70, 90, 15, 65),
-('Thanos com todas as joias', 40, 100, 120, 150, 180, 1, 100),
 ('Fauce de Ébano', 11, 62, 58, 79, 43, 12, 84),
 ('Corvus Glaive', 83, 72, 96, 57, 41, 9, 46),
 ('Próxima Meia-Noite', 15, 98, 77, 69, 92, 2, 33),
@@ -10,3 +9,5 @@ INSERT INTO vilao (nome, agilidade, vida, experiencia, dano_maximo, dano_critico
 ('Outriders', 47, 77, 51, 74, 24, 18, 40),
 ('Sakaarans', 81, 37, 89, 25, 82, 8, 40),
 ('Capitão américa (passado)', 18, 59, 82, 20, 92, 2, 42),
+('Gavião Arqueiro', 18, 59, 82, 20, 92, 2, 42),
+('Viúva Negra', 18, 59, 82, 20, 92, 2, 42);
