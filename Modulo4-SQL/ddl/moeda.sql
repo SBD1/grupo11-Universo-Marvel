c1f5986509bef2eff29e85b7f5e6db9c75b9e43b
Julio Litwin <j.litwin@live.com>
2022-03-28 08:34:08 -0300

    adicionado o mock para moeda

---
+++ Modulo4-SQL/ddl/moeda.sql
@@ -0,0 +1,4 @@
+INSERT INTO MOEDA (nome, descricao) VALUES
+('Ouro', 'O ouro é um metal nobre, de elevada densidade e baixa dureza.'),
+('Prata', 'O prata é um metal nobre, é um subproduto da mineração de chumbo e está frequentemente associada ao cobre. Dentre os metais, é a que mais conduz corrente elétrica, superando o cobre.'),
+('Cobre', 'É um dos metais mais importantes industrialmente, de coloração avermelhada, dúctil, maleável e bom condutor de eletricidade. ')
