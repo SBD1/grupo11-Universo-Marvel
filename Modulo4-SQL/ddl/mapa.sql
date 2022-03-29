76e9456e9ec8874284ab24f192a9ab10e25614cd
hericklima22 <herick.lima77@gmail.com>
2022-03-28 21:28:01 -0300

    alteração do nome da pasta mãe
    Co-authored-by: Pedro Haick <peHaick@users.noreply.github.com>
    Co-authored-by: Carlos Rafael  <carlosoftwar@gmail.com>

--- Modulo4-SQL/ddl/mapa.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO mapa (nome, ano, altura, largura) VALUES
-('Asgard', 2013, 67, 83),
-('Nova York', 2012, 89, 51),
-('Base da S.H.I.E.L.D.', 1970, 66, 88),
-('Vormir', 2014, 91, 55),
-('Morag', 2014, 61, 78);
