bd6826c4f1b7b6aec5f44d56adafe68fafae85f9
Helena Goulart <helenabretasg@gmail.com>
2022-04-24 01:16:57 -0300

    Inclusao do personagem dentro da nova interface

--- Game/interface/Interface.py
+++ Game/interface/Interface.py
@@ -1,12 +1,18 @@
 import curses
 from curses import textpad
 
-def main (stdscr):
+def main(mywindow):
     curses.curs_set(0)
-    sh, sw = stdscr.getmaxyx()
+    sh, sw = mywindow.getmaxyx()
     box = [[3,3], [sh-3, sw-3]]
-    textpad.rectangle(stdscr, box[0][0], box[0][1], box[1][0], box[1][1])
-    stdscr.refresh()
-    stdscr.getch()
-    
+    textpad.rectangle(mywindow, box[0][0], box[0][1], box[1][0], box[1][1])
+
+    personagem = [[sh//2, sw//2-1]]
+    direction = curses.KEY_RIGHT
+
+    for y,x in personagem:
+        mywindow.addstr(y, x, '#')
+
+    mywindow.getch()
+
 curses.wrapper(main)
